0dc14c1e0557a0385aef203fb1654ba4f7f0a594
unknown <andy.pope.13@gmail.com>
2019-08-04 17:16:48 -0700

    Preparation

--- mk.bat
+++ mk.bat
@@ -2,10 +2,12 @@
 
 
 del .\dist\Station.exe
-pyinstaller -F -n Station .\comWindow.py
+.\venv\Scripts\pyinstaller.exe -F -n Station .\comWindow.py
 
 xcopy .\comWindow.ui .\dist\ /y
 xcopy .\main.ui .\dist\ /y
+xcopy .\marker.png .\dist\ /y
+xcopy .\tiles\* .\dist\tiles\ /e /i /y
 
 
 PAUSE

7a9caf4d9867f536aab7cc862448db5ca7b3b39b
DSep <dyanatkar@me.com>
2020-03-22 22:59:49 -0700

    bulk sensor mocking in debug connection

--- DebugConnection.py
+++ DebugConnection.py
@@ -21,9 +21,27 @@ class DebugConnection(IConnection):
 
             ba = bytearray(struct.pack("f", value))
 
-            return b"X"+ba
+            bulk_sensor_arr = self.bulk_sensor_mock_random()
+            return bulk_sensor_arr
+            # return b"X"+bulk_sensor_arr
         else:
             return None
+
+    def bulk_sensor_mock_random(self):
+        bulk_sensor_arr = bytearray()
+        bulk_sensor_arr.append(0x30)  # id
+        bulk_sensor_arr.extend(random.randint(0, 1e9).to_bytes(length=4, byteorder='big'))  # time
+        bulk_sensor_arr.extend(struct.pack("f", random.uniform(0, 1e6)))
+        bulk_sensor_arr.extend(struct.pack("f", random.uniform(0, 1e6)))
+        bulk_sensor_arr.extend(struct.pack("f", random.uniform(0, 1e6)))
+        bulk_sensor_arr.extend(struct.pack("f", random.uniform(0, 1e6)))
+        bulk_sensor_arr.extend(struct.pack("f", random.uniform(0, 1e6)))
+        bulk_sensor_arr.extend(struct.pack("f", random.uniform(0, 1e6)))
+        bulk_sensor_arr.extend(struct.pack("f", random.uniform(0, 1e6)))
+        bulk_sensor_arr.extend(struct.pack("f", random.uniform(0, 1e6)))
+        bulk_sensor_arr.extend(struct.pack("f", random.uniform(0, 1e6)))
+        bulk_sensor_arr.extend(random.randint(0, 100).to_bytes(length=1, byteorder='big'))  # state
+        return bulk_sensor_arr
 
     def send(self, data):
         pass

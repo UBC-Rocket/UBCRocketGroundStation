90a09ab013af1b3a8f980752a9597846d5896e73
DSep <dyanatkar@me.com>
2020-07-05 23:56:19 -0700

    Fixed header bug + added better incrementing so indices are not hardcoded numbers

--- RadioController.py
+++ RadioController.py
@@ -53,7 +53,10 @@ class RadioController:
         # data extraction
         data_unit = byte_list[header.header_length:header.total_length]
         data_length = header.total_length - header.header_length
+        # print(data_unit)
         parsed_data: Dict[any, any] = self.parse_data(header.subpacket_id, data_unit, data_length)
+        # Add timestamp from header
+        parsed_data[SubpacketEnum.TIME.value] = header.timestamp
         return parsed_data, header.total_length
 
 
@@ -68,31 +71,30 @@ class RadioController:
 
 
     # general data parser interface
-    def parse_data(self, type_id, byte_list, length):
+    def parse_data(self, type_id, byte_list, length) -> Dict[any, any]:
         return self.packetTypeToParser[type_id](self, byte_list, length)
 
     # Header extractor helper
     def header(self, byte_list: List) -> Header:
-        currByte = 0 # index for which byte is to be processed next. Collected in return as header size
+        currByte = Count(0, 1) # index for which byte is to be processed next. Collected in return as header size
         # Get ID
-        subpacket_id: int = self.extract_subpacket_ID(byte_list[currByte])
-        currByte += 1
+        subpacket_id: int = self.extract_subpacket_ID(byte_list[currByte.currAndInc(1)])
         # Get timestamp
         # TODO REVIEW/CHANGE in type refactoring: how this is required to convert from List[bytes] to List[int]
-        timestamp_int_list: List[int] = [int(x[0]) for x in byte_list[currByte: currByte + 4]]
+        timestamp_int_list: List[int] = [int(x[0]) for x in byte_list[currByte.curr(): currByte.next(4)]]
         timestamp: int = self.fourtofloat(timestamp_int_list)
-        currByte += 4
         # Get length
         if isPacketLengthConst(subpacket_id):
             length: int = PACKET_ID_TO_CONST_LENGTH[subpacket_id]
         else:
-            length: int = int.from_bytes(byte_list[currByte], "big")
-            currByte += 1
-        return Header(subpacket_id, timestamp, currByte, length)
+            length: int = int.from_bytes(byte_list[currByte.currAndInc(1)], "big")
+        return Header(subpacket_id, timestamp, currByte.curr(), length)
 
     ### General sensor data parsers
 
-    def status_ping(self, byte_list, length):  # TODO
+    def statusPing(self, byte_list, length):  # TODO
+        mask = 0b00000011
+        overallStatus = byte_list[0] & mask
         converted = {0.0}  # STUB
         return converted
 
@@ -125,24 +127,24 @@ class RadioController:
 
         # TODO REVIEW/CHANGE in type refactoring: how this is required to convert from List[bytes] to List[int]
         int_list: List[int] = [int(x[0]) for x in byte_list]
+        currByte = Count(0, 4)
 
-        data[SubpacketEnum.TIME.value] = self.fourtoint(int_list[0:4])
-        data[SubpacketEnum.CALCULATED_ALTITUDE.value] = self.fourtofloat(int_list[4:8])  # TODO Double check it is calculated barometer altitude with firmware
-        data[SubpacketEnum.ACCELERATION_X.value] = self.fourtofloat(int_list[8:12])
-        data[SubpacketEnum.ACCELERATION_Y.value] = self.fourtofloat(int_list[12:16])
-        data[SubpacketEnum.ACCELERATION_Z.value] = self.fourtofloat(int_list[16:20])
-        data[SubpacketEnum.ORIENTATION_1.value] = self.fourtofloat(int_list[20:24])
-        data[SubpacketEnum.ORIENTATION_2.value] = self.fourtofloat(int_list[24:28])
-        data[SubpacketEnum.ORIENTATION_3.value] = self.fourtofloat(int_list[28:32])
-        data[SubpacketEnum.LATITUDE.value] = self.fourtofloat(int_list[32:36])
-        data[SubpacketEnum.LONGITUDE.value] = self.fourtofloat(int_list[36:40])
-        data[SubpacketEnum.STATE.value] = int_list[40]
+        data[SubpacketEnum.CALCULATED_ALTITUDE.value] = self.fourtofloat(int_list[currByte.curr():currByte.next(4)])  # TODO Double check it is calculated barometer altitude with firmware
+        data[SubpacketEnum.ACCELERATION_X.value] = self.fourtofloat(int_list[currByte.curr():currByte.next(4)])
+        data[SubpacketEnum.ACCELERATION_Y.value] = self.fourtofloat(int_list[currByte.curr():currByte.next(4)])
+        data[SubpacketEnum.ACCELERATION_Z.value] = self.fourtofloat(int_list[currByte.curr():currByte.next(4)])
+        data[SubpacketEnum.ORIENTATION_1.value] = self.fourtofloat(int_list[currByte.curr():currByte.next(4)])
+        data[SubpacketEnum.ORIENTATION_2.value] = self.fourtofloat(int_list[currByte.curr():currByte.next(4)])
+        data[SubpacketEnum.ORIENTATION_3.value] = self.fourtofloat(int_list[currByte.curr():currByte.next(4)])
+        data[SubpacketEnum.LATITUDE.value] = self.fourtofloat(int_list[currByte.curr():currByte.next(4)])
+        data[SubpacketEnum.LONGITUDE.value] = self.fourtofloat(int_list[currByte.curr():currByte.next(4)])
+        data[SubpacketEnum.STATE.value] = int_list[currByte.currAndInc(1)]
         return data
 
 
     # Dictionary of subpacket id - function to parse that data
-    packetTypeToParser: Dict[int, Callable[[list, int], Any]] = {  # TODO review this type hint
-        SubpacketEnum.STATUS_PING.value: status_ping,
+    packetTypeToParser: Dict[int, Callable[[list, int], Dict[any, any]]] = {  # TODO review this type hint
+        SubpacketEnum.STATUS_PING.value: statusPing,
         SubpacketEnum.MESSAGE.value: message,
         SubpacketEnum.EVENT.value: event,
         SubpacketEnum.CONFIG.value: config,
@@ -166,3 +168,36 @@ class RadioController:
         b = struct.pack('4B', *data)
         c = struct.unpack('>I' if self.bigEndianInts else '<I', b)
         return c[0]
+
+
+# python way of doing ++ (unlimited incrementing) TODO Put this in utils folder/file?
+class Count:
+
+    def __init__(self, start=0, interval=1):
+        self.interval = interval
+        self.num = start
+
+    def __iter__(self):
+        return self
+
+    def curr(self):
+        return self.num
+
+    # increments and returns the new value
+    def next(self, interval=0):
+        if interval == 0:
+            self.num += self.interval
+        else:
+            self.num += interval
+
+        return self.num
+
+    # increments and returns the new value
+    def currAndInc(self, interval=9):
+        num = self.num
+        if interval == 0:
+            self.num += self.interval
+        else:
+            self.num += interval
+
+        return num

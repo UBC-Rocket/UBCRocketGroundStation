144f5c1429d158652e05c50cfd7c2ada5545f0b0
Jesse Li <jesse.li2002@gmail.com>
2020-09-06 13:00:25 -0700

    Specified HW sim in tantalus rocket profile

--- profiles/rockets/tantalus.py
+++ profiles/rockets/tantalus.py
@@ -1,7 +1,15 @@
-from ..label import (Label, update_acceleration, update_altitude, update_gps,
-                     update_max_altitude, update_pressure, update_state,
-                     update_test_separation)
+from ..label import (
+    Label,
+    update_acceleration,
+    update_altitude,
+    update_gps,
+    update_max_altitude,
+    update_pressure,
+    update_state,
+    update_test_separation,
+)
 from ..rocket_profile import RocketProfile
+from ...connections.sim.hw_sim import HWSim
 
 tantalus_labels = [
     Label("Altitude", update_altitude),
@@ -13,4 +21,20 @@ tantalus_labels = [
     Label("TestSeparation", update_test_separation, "Test Separation"),
 ]
 
-tantalus = RocketProfile({"Arm": "arm", "Status": "status"}, tantalus_labels)
+_MAIN_IGN_TEST = 4
+_MAIN_IGN_READ = 14
+_MAIN_IGN_FIRE = 16
+_DROGUE_IGN_TEST = 17
+_DROGUE_IGN_READ = 34
+_DROGUE_IGN_FIRE = 35
+
+
+_hw_sim = HWSim(
+    [
+        (_MAIN_IGN_TEST, _MAIN_IGN_READ, _MAIN_IGN_FIRE),
+        (_DROGUE_IGN_TEST, _DROGUE_IGN_READ, _DROGUE_IGN_FIRE),
+    ]
+)
+
+tantalus = RocketProfile({"Arm": "arm", "Status": "status"}, tantalus_labels, _hw_sim)
+

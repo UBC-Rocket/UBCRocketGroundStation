cf23845ae5728361b19c03761cb80c7419a8ec3f
alxshev <ashevchuk2001@gmail.com>
2020-10-18 10:49:15 -0700

    added code to throw exception when multiple build files are found

--- connections/sim/sim_connection_factory.py
+++ connections/sim/sim_connection_factory.py
@@ -33,10 +33,10 @@ class SimConnectionFactory(ConnectionFactory):
 
         # Check FW (child) dir and FLARE (neighbour) dir for rocket build files
         # If multiple build files found throw exception
-        flare = self._getParentDir(LOCAL) + "/FLARE/avionics/build/"
-        neighbourBuildFiles = [s for s in os.listdir(flare) if rocket.rocket_name.lower() in s.lower()]
+        flarePath = self._getParentDir(LOCAL) + "/FLARE/avionics/build/"
+        neighbourBuildFiles = [s for s in os.listdir(flarePath) if rocket.rocket_name.lower() in s.lower()]
 
-        childBuildFileExists = "program" in os.listdir('.')
+        childBuildFileExists = "program" in os.listdir('./FW')
         neighbourBuildFileExists = bool(neighbourBuildFiles)
 
         if childBuildFileExists and neighbourBuildFileExists:
@@ -44,7 +44,7 @@ class SimConnectionFactory(ConnectionFactory):
                 f"Multiple build files found: {neighbourBuildFiles[0]} and program")
         elif neighbourBuildFileExists:
             executableName = neighbourBuildFiles[0]
-            path = flare
+            path = flarePath
         elif childBuildFileExists:
             executableName = "program"
             path = os.path.join(LOCAL, FIRMWARE_DIR)

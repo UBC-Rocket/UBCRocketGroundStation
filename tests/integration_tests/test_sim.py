8beea68ca75b259fe9626c66743aa0220b387363
Andy <7855965+SilentSys@users.noreply.github.com>
2020-11-10 17:02:26 -0800

    Added hw sim for remaining sensors & tests

--- tests/integration_tests/test_sim.py
+++ tests/integration_tests/test_sim.py
@@ -21,9 +21,11 @@ def main_app() -> MainApp:
     yield app  # Provides app, following code is run on cleanup
     app.shutdown()
 
+
 def set_dummy_sensor_values(hw, sensor_type: SensorType, *vals):
     with hw.lock:
         hw._sensors[sensor_type].set_value(tuple(vals))
+
 
 def wait_new_bundle():
     # Wait a few update cycles to flush any old packets out
@@ -33,8 +35,8 @@ def wait_new_bundle():
         assert BULK_SENSOR_EVENT.wait(snapshot) >= 1
         assert BUNDLE_ADDED_EVENT.wait(snapshot) >= 1
 
-def test_arming(qtbot, main_app):
 
+def test_arming(qtbot, main_app):
     wait_new_bundle()
     assert main_app.rocket_data.lastvalue(SubpacketEnum.STATE.value) == 0
 
@@ -69,6 +71,7 @@ def test_gps_read(qtbot, main_app):
         assert main_app.rocket_data.lastvalue(SubpacketEnum.LATITUDE.value) == vals[0]
         assert main_app.rocket_data.lastvalue(SubpacketEnum.LONGITUDE.value) == vals[1]
         assert main_app.rocket_data.lastvalue(SubpacketEnum.GPS_ALTITUDE.value) == vals[2]
+
 
 def test_baro_altitude(qtbot, main_app):
     Pb = 101325
@@ -110,8 +113,68 @@ def test_baro_altitude(qtbot, main_app):
 
         assert main_app.rocket_data.lastvalue(SubpacketEnum.PRESSURE.value) == vals[0]
         assert main_app.rocket_data.lastvalue(SubpacketEnum.BAROMETER_TEMPERATURE.value) == vals[1]
-        assert main_app.rocket_data.lastvalue(SubpacketEnum.CALCULATED_ALTITUDE.value) == approx(altitude(vals[0]) - altitude(ground_pres), 0.1)
+        assert main_app.rocket_data.lastvalue(SubpacketEnum.CALCULATED_ALTITUDE.value) == approx(
+            altitude(vals[0]) - altitude(ground_pres), 0.1)
         assert main_app.rocket_data.lastvalue(SubpacketEnum.BAROMETER_TEMPERATURE.value) == vals[1]
+
+
+def test_accelerometer_read(qtbot, main_app):
+    connection = main_app.connection
+    hw = connection._hw_sim
+
+    test_vals = [
+        (1, 0, 0),
+        (0, 1, 0),
+        (0, 0, 1),
+    ]
+
+    for vals in test_vals:
+        set_dummy_sensor_values(hw, SensorType.ACCELEROMETER, *vals)
+        wait_new_bundle()
+
+        assert main_app.rocket_data.lastvalue(SubpacketEnum.ACCELERATION_X.value) == vals[0]
+        assert main_app.rocket_data.lastvalue(SubpacketEnum.ACCELERATION_Y.value) == vals[1]
+        assert main_app.rocket_data.lastvalue(SubpacketEnum.ACCELERATION_Z.value) == vals[2]
+
+
+def test_imu_read(qtbot, main_app):
+    connection = main_app.connection
+    hw = connection._hw_sim
+
+    test_vals = [
+        (1, 0, 0, 0),
+        (0, 1, 0, 0),
+        (0, 0, 1, 0),
+        (0, 0, 0, 1),
+    ]
+
+    for vals in test_vals:
+        set_dummy_sensor_values(hw, SensorType.IMU, *vals)
+        wait_new_bundle()
+
+        assert main_app.rocket_data.lastvalue(SubpacketEnum.ORIENTATION_1.value) == vals[0]
+        assert main_app.rocket_data.lastvalue(SubpacketEnum.ORIENTATION_2.value) == vals[1]
+        assert main_app.rocket_data.lastvalue(SubpacketEnum.ORIENTATION_3.value) == vals[2]
+
+
+def test_temperature_read(qtbot, main_app):
+    connection = main_app.connection
+    hw = connection._hw_sim
+
+    test_vals = [
+        (0,),
+        (10,),
+        (20,),
+    ]
+
+    for vals in test_vals:
+        set_dummy_sensor_values(hw, SensorType.TEMPERATURE, *vals)
+        wait_new_bundle()  # Just to wait a few cycles for the FW to read from HW sim
+        snapshot = get_event_stats_snapshot()
+        main_app.sendCommand("TEMP")
+        assert SINGLE_SENSOR_EVENT.wait(snapshot) == 1
+
+        assert main_app.rocket_data.lastvalue(SubpacketEnum.TEMPERATURE.value) == vals[0]
 
 
 def test_clean_shutdown(qtbot, main_app):

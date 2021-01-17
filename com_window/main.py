6b312080cc63aa8e481c27cf5dd221466c5567a6
Andy <7855965+SilentSys@users.noreply.github.com>
2021-01-16 20:29:37 -0800

    Resize com window to natural size

--- com_window/main.py
+++ com_window/main.py
@@ -55,6 +55,7 @@ class ComWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         comlist = list(map(lambda x: x.device, serial.tools.list_ports.comports()))
         self.comBox.addItems(comlist)
 
+        self.resize(self.sizeHint())
         self.setFixedSize(self.size())
 
     def doneButtonPressed(self) -> None:

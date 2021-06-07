7306896a6980fa5c6a3bd25ca7049fe0137eddc0
DSep <dyanatkar@me.com>
2021-06-06 22:17:28 -0700

    Small scripting mistake

--- ci.sh
+++ ci.sh
@@ -49,11 +49,8 @@ cd UBCRocketGroundStation
 # Unit tests & integration tests
 source venv/bin/activate
 mkdir test-reports
-cd test_reports/coverage_reports
-cd ..
+cd test_reports/
 coverage run --omit 'venv/*' -m pytest tests/*.py && coverage report --omit 'venv/*' && coverage xml -o test_reports/coverage.xml
-# coverage run --omit 'venv/*' -m pytest tests/*.py && coverage report --omit 'venv/*' && coverage html -d test_reports/coverage_reports
-
 deactivate
 
 # Pyinstaller "build" test & GS self-test

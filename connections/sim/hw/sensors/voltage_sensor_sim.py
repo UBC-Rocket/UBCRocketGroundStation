d7b5673d22ecc6cfff6192c4a20e092267db26f2
LAPTOP-LIPD8R0B\Jords <jordanschneider@telus.net>
2021-07-12 20:13:42 -0700

    Created new VoltageSensor class with dummy read_data method to be used during simulations

---
+++ connections/sim/hw/sensors/voltage_sensor_sim.py
@@ -0,0 +1,27 @@
+
+class VoltageSensor:
+    """
+    Simulates the hardware used for measuring battery voltage.
+    """
+
+    NOMINAL_VOLTAGE = 5
+
+    def __init__(self, pin: int):
+        """
+        In firmware's usage, to simulate the sensor used to read the battery's voltage
+        :param pin The pin whose value will be read from and which will be converted to a voltage level
+        """
+
+        self._pin = pin
+
+    def read_data(self):
+        """
+        :brief: Simulate voltage level from output pin.
+        :return: Voltage level obtained by mapping analogRead value onto a voltage level.
+        :note: currently a dummy implementation that returns a constant value
+        """
+
+        return self.NOMINAL_VOLTAGE
+
+
+

5cb3324cf99b1cf487dd90b155838d84f371eb44
Marco Yuen <65629579+MarcoYuen17@users.noreply.github.com>
2022-10-27 19:34:50 -0700

    check available python versions

--- ci.sh
+++ ci.sh
@@ -20,6 +20,7 @@ export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 
 # Start setting up Python for GS
 sudo apt-get install -y tk-dev
+pyenv install --list
 env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.10.6
 pyenv global 3.10.6
 python --version

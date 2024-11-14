796cec2132d3688c71dfd24d6b946b39b6d8f94c
Jason <jasonkwok375@gmail.com>
2024-11-14 11:57:55 -0800

    Fixed CircleCi Tests
    Updated package versions, python to 3.12, ubuntu image version, and codecov  /circlecir dir interference

--- ci.sh
+++ ci.sh
@@ -9,6 +9,9 @@ set -o verbose
 
 sudo apt-get update -y
 
+# Install missing packages for qt - libxcb-iccm4.so not found
+sudo apt-get install libxcb-xinerama0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xkb1 libxcb-shape0 libxkbcommon-x11-0
+
 # Needed for unit testing with qt https://github.com/pytest-dev/pytest-qt/issues/293
 sudo apt-get install -y xvfb libxkbcommon-x11-0
 sudo Xvfb :1 -screen 0 1024x768x24 </dev/null &
@@ -20,8 +23,8 @@ export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 
 # Start setting up Python for GS
 sudo apt-get install -y tk-dev
-env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.7.9
-pyenv global 3.7.9
+env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install -s 3.12
+pyenv global 3.12
 python --version
 python -m pip install --upgrade pip setuptools wheel
 
@@ -47,20 +50,21 @@ cd ../../..
 cd UBCRocketGroundStation
 
 # Unit tests & integration tests
-source venv/bin/activate
+source .venv/bin/activate
 mkdir test_reports
+mkdir test_coverage
 
 # Unit
-coverage run --omit 'venv/*' -m pytest --durations=0 --junitxml=test_reports/unit-test-results.xml --ignore=tests/integration_tests tests
-coverage report --omit 'venv/*'
-coverage xml -o test_reports/unit-test-coverage.xml
-head test_reports/unit-test-coverage.xml
+coverage run --omit '.venv/*' -m pytest --durations=0 --junitxml=test_reports/unit-test-results.xml --ignore=tests/integration_tests tests
+coverage report --omit '.venv/*'
+coverage xml -o test_coverage/unit-test-coverage.xml
+head test_coverage/unit-test-coverage.xml
 
 # Integration
-coverage run --omit 'venv/*' -m pytest --durations=0 --junitxml=test_reports/integ-test-results.xml tests/integration_tests
-coverage report --omit 'venv/*'
-coverage xml -o test_reports/integ-test-coverage.xml
-head test_reports/integ-test-coverage.xml
+coverage run --omit '.venv/*' -m pytest --durations=0 --junitxml=test_reports/integ-test-results.xml tests/integration_tests
+coverage report --omit '.venv/*'
+coverage xml -o test_coverage/integ-test-coverage.xml
+head test_coverage/integ-test-coverage.xml
 
 deactivate
 

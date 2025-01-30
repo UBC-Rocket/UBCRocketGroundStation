d9a8949856c76f2e47757cb97ddb2a056ecb4acf
Jason <jasonkwok375@gmail.com>
2025-01-30 15:44:54 -0800

    Removed circular dependency
    Circular dependency that called a half initialized module

--- main_window/main_app.py
+++ main_window/main_app.py
@@ -8,7 +8,6 @@ from PyQt5.QtCore import pyqtSignal
 
 from connections.connection import Connection
 from util.detail import LOGS_DIR, SESSION_ID, LOGGER
-from profiles.rocket_profile import RocketProfile
 
 from main_window.read_thread import ReadThread
 from main_window.rocket_data import RocketData
@@ -22,7 +21,7 @@ class MainApp(QtWidgets.QMainWindow):
     """Manage main app"""
     sig_send = pyqtSignal(str)
 
-    def __init__(self, connections: Dict[str, Connection], rocket_profile: RocketProfile) -> None:
+    def __init__(self, connections: Dict[str, Connection], rocket_profile) -> None:
         """
 
         :param connections:
